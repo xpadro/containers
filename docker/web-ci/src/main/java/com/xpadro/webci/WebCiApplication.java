56934d843cbd2c81e02405cc07a33dbdab2089a8
Xavier Padro <xpadro.sw@gmail.com>
2019-12-03 08:33:28 +0100

    First version of webapp

---
+++ docker/web-ci/src/main/java/com/xpadro/webci/WebCiApplication.java
@@ -0,0 +1,21 @@
+package com.xpadro.webci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class WebCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebCiApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String home() {
+		return "This is a test";
+	}
+
+}
---
+++ docker/web-ci/src/test/java/com/xpadro/webci/WebCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xpadro.webci;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
